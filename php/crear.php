6e8ac1dd9973b0bae2d3c2191615489d7b6dab07
John Alberto Parrado Gordillo <parradojohn1421727@gmail.com>
2012-07-05 13:41:58 -0500

    Update master

--- php/crear.php
+++ php/crear.php
@@ -11,7 +11,7 @@ $ext="form".$m['name'];
             <label class="control-label" for="<?PHP echo($ext); ?>-usuario">Usuario</label>
             <div class="controls">
                 <div class="input-append">
-                    <input type="text" value="johnparrado" class="input-xlarge focused" placeholder="Ej: johnDoes" name="<?PHP echo($ext); ?>-usuario" id="<?PHP echo($ext); ?>-usuario" required="required" maxlength=30 minlength=5 pattern="^[a-zA-Z][a-zA-Z0-9-_\.]{4,30}$">
+                    <input type="text" value="" class="input-xlarge focused" placeholder="Ej: johnDoes" name="<?PHP echo($ext); ?>-usuario" id="<?PHP echo($ext); ?>-usuario" required="required" maxlength=30 minlength=5 pattern="^[a-zA-Z][a-zA-Z0-9-_\.]{4,30}$">
                     <a href="#" class="toltip" rel="tooltip" data-original-title="Mínimo 5 caracteres, maximo 30."><span class="add-on"><i class="icon-exclamation-sign"></i></span></a>
                 </div>
             </div>
@@ -20,7 +20,7 @@ $ext="form".$m['name'];
             <label class="control-label" for="<?PHP echo($ext); ?>-contra">Contraseña</label>
             <div class="controls">
                 <div class="input-append">
-                    <input type="password" value="CDE34rfv" class="input-xlarge focused" placeholder="Ej: hjk123PHD" name="<?PHP echo($ext); ?>-contra" id="<?PHP echo($ext); ?>-contra" required="required" maxlength=30 minlength=5 pattern="(?!^[0-9]*$)(?!^[a-zA-Z]*$)^([a-zA-Z0-9]{4,30})$">
+                    <input type="password" value="" class="input-xlarge focused" placeholder="Ej: hjk123PHD" name="<?PHP echo($ext); ?>-contra" id="<?PHP echo($ext); ?>-contra" required="required" maxlength=30 minlength=5 pattern="(?!^[0-9]*$)(?!^[a-zA-Z]*$)^([a-zA-Z0-9]{4,30})$">
                     <a href="#" class="toltip" rel="tooltip" data-original-title="Mínimo 5 caracteres, maximo 30, por lo menos un digito y un alfanumérico, y no puede contener caracteres espaciales."><span class="add-on"><i class="icon-exclamation-sign"></i></span></a>
                 </div>
             </div>
@@ -29,7 +29,7 @@ $ext="form".$m['name'];
             <label class="control-label" for="<?PHP echo($ext); ?>-email">Email</label>
             <div class="controls">
                 <div class="input-append">
-                    <input type="email" value="johnparrado@gmail.com" class="input-xlarge focused" placeholder="Ej: johndoes@email.com" name="<?PHP echo($ext); ?>-email" id="<?PHP echo($ext); ?>-email" required="required" maxlength=40 minlength=5 pattern="^[a-z]+@[a-z]+\.[a-z]{2,4}$">
+                    <input type="email" value="" class="input-xlarge focused" placeholder="Ej: johndoes@email.com" name="<?PHP echo($ext); ?>-email" id="<?PHP echo($ext); ?>-email" required="required" maxlength=40 minlength=5 pattern="^[a-z]+@[a-z]+\.[a-z]{2,4}$">
                     <a href="#" class="toltip" rel="tooltip" data-original-title="Mínimo 5 caracteres, maximo 40."><span class="add-on"><i class="icon-exclamation-sign"></i></span></a>
                 </div>
             </div>
